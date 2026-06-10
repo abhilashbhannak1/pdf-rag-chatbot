c003ee228beb24e096e3bfc7bb62774f9d147326
abhilashbhannak1 <bhannakabhilash@gmail.com>
2026-06-10 15:49:49 +0530

    updated chunker

--- src/chunker.py
+++ src/chunker.py
@@ -1,5 +1,7 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
+from loader import load_pdf
+
 
 def create_chunks(documents):
     """
@@ -14,3 +16,20 @@ def create_chunks(documents):
     chunks = splitter.split_documents(documents)
 
     return chunks
+if __name__ == "__main__":
+
+    docs = load_pdf("data/22211A0431_BHANNAK_ABHILASH_.pdf")
+
+    chunks = create_chunks(docs)
+
+    print("Total Chunks:", len(chunks))
+
+for i, chunk in enumerate(chunks):
+    print(f"\n{'='*60}")
+    print(f"CHUNK {i+1}")
+    print(f"{'='*60}")
+
+    print(chunk.page_content)
+
+    print("\nMetadata:")
+    print(chunk.metadata)
