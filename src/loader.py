1284ab07b5b91efabbf4bf5b3314304702173ddf
abhilashbhannak1 <bhannakabhilash@gmail.com>
2026-06-10 14:51:14 +0530

    Updated loader file

--- src/loader.py
+++ src/loader.py
@@ -9,5 +9,13 @@ def load_pdf(pdf_path: str):
     loader = PyPDFLoader(pdf_path)
 
     documents = loader.load()
-
     return documents
+if __name__ == "__main__":
+    docs = load_pdf("data/22211A0431_BHANNAK_ABHILASH_.pdf")
+
+    print("Total Pages:", len(docs))
+    print("\nFirst Page Content:")
+    print(docs[0].page_content)
+
+    print("\nMetadata:")
+    print(docs[0].metadata)
